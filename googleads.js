654a142cfe012e519ec693407d76d4a26eb1d2aa
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-06-25 19:28:32 +0900

    Add googleads.js

---
+++ googleads.js
@@ -0,0 +1,9 @@
+// Google AdSense Configuration
+(function() {
+  var s = document.createElement('script');
+  s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+  s.setAttribute('data-ad-client', 'ca-pub-5525538810839147');
+  s.async = true;
+  s.crossOrigin = 'anonymous';
+  document.head.appendChild(s);
+})();
